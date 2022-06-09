19ac6d1f52cd025508bc7c4cfd2ff7f3317d4132
Michael Lagomarsino <mike@radom.network>
2022-06-09 14:48:04 +0100

    Restructure to use library for integration testing

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,7 @@
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
-
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
-}
-
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
-}
+use rust_api_tutorial_3::run;
 
 // #[actix_web::main] // or #[tokio::main]
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-    .bind("127.0.0.1:8000")?
-    .run()
-    .await
+    run().await
 }
