f7b50605af43b5117c4f3221b4edb63a7a863de5
Michael Lagomarsino <mike@radom.network>
2022-06-09 15:47:11 +0100

    Random port - use TcpListener

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use rust_api_tutorial_3::run;
+use std::net::TcpListener;
 
 // #[actix_web::main] // or #[tokio::main]
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     // Bubble up the io::Error if we failed to bind the address
     // Otherwise call .await on our Server
-    run("127.0.0.1:8000")?.await
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind to port 8000");
+    run(listener)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,3 +1,5 @@
+use std::net::TcpListener;
+
 // `tokio::test` is the testing equivalent of `tokio::main`.
 // It also spares you from having to specify the `#[test]` attribute.
 //
@@ -6,17 +8,17 @@
 #[tokio::test]
 async fn health_check_works() {
     // Arrange
-    spawn_app();
+    let address = spawn_app();
     // We need to bring in `reqwest` 
     // to perform HTTP requests against our application.
     let client = reqwest::Client::new();
 
     // Act
     let response = client
-            .get("http://127.0.0.1:8000/health_check")
-            .send()
-            .await
-            .expect("Failed to execute request.");
+        .get(&format!("{}/health_check", &address))
+        .send()
+        .await
+        .expect("Failed to execute request.");
 
     // Assert
     assert!(response.status().is_success());
@@ -27,10 +29,15 @@ async fn health_check_works() {
 // We are also running tests, so it is not worth it to propagate errors:
 // if we fail to perform the required setup we can just panic and crash
 // all the things.
-fn spawn_app() {
-    let server = rust_api_tutorial_3::run("127.0.0.1:0").expect("Failed to bind address");
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    // We retrieve the port assigned to us by the OS
+    let port = listener.local_addr().unwrap().port();
+    let server = rust_api_tutorial_3::run(listener).expect("Failed to bind address");
     // Launch the server as a background task
     // tokio::spawn returns a handle to the spawned future,
     // but we have no use for it here, hence the non-binding let
     let _ = tokio::spawn(server);
+    // We return the application address to the caller!
+    format!("http://127.0.0.1:{}", port)
 }
