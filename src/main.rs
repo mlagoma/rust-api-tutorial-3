c859d84b8f9e9dc584afc36182b63cd186349d2b
Michael Lagomarsino <mike@radom.network>
2022-06-09 15:07:41 +0100

    First integration test

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,7 @@ use rust_api_tutorial_3::run;
 // #[actix_web::main] // or #[tokio::main]
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    run().await
+    // Bubble up the io::Error if we failed to bind the address
+    // Otherwise call .await on our Server
+    run()?.await
 }
