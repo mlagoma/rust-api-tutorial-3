b238981c5f0ad5db2bc7cf75c154c271c3a73919
Michael Lagomarsino <mike@radom.network>
2022-06-18 02:10:53 +0100

    Add db connection to app

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use rust_api_tutorial_3::startup::run;
 use rust_api_tutorial_3::configuration::get_configuration;
 
+use sqlx::{Connection, PgConnection};
 use std::net::TcpListener;
 
 
@@ -9,10 +10,13 @@ use std::net::TcpListener;
 async fn main() -> std::io::Result<()> {
     // Panic if we can't read configuration
     let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection = PgConnection::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
     // We have removed the hard-coded `8000` - it's now coming from our settings!
     let address = format!("127.0.0.1:{}", configuration.application_port);
     // Bubble up the io::Error if we failed to bind the address
     // Otherwise call .await on our Server
     let listener = TcpListener::bind(address)?;
-    run(listener)?.await
+    run(listener, connection)?.await
 }
--- src/startup.rs
+++ src/startup.rs
@@ -2,20 +2,28 @@ use crate::routes::{health_check, subscribe};
 
 use actix_web::{web, App, HttpResponse, HttpServer};
 use actix_web::dev::Server;
+use sqlx::PgConnection;
 use std::net::TcpListener;
 
 // Notice the different signature!
 // We return `Server` on the happy path and we dropped the `async` keyword
 // We have no .await call, so it is not needed anymore.
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
+pub fn run(
+    listener: TcpListener,
+	// New parameter!
+    connection: PgConnection
+) -> Result<Server, std::io::Error> {
+    // Wrap the connection in a smart pointer
+    let connection = web::Data::new(connection);
+    // Capture `connection` from the surrounding environment
+    let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(health_check))
-            // A new entry in our routing table for POST /subscriptions requests
             .route("/subscriptions", web::post().to(subscribe))
+            // Get a pointer copy and attach it to the application state
+            .app_data(connection.clone())
     })
     .listen(listener)?
     .run();
-    // No .await here!
     Ok(server)
 }
