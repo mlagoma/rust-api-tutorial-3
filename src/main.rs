8273337a1412cda3c00795dbb7ae1928e1a7e15c
Michael Lagomarsino <mike@radom.network>
2022-06-18 17:38:15 +0100

    Add logging

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,15 @@ use rust_api_tutorial_3::configuration::get_configuration;
 
 use sqlx::PgPool;
 use std::net::TcpListener;
-
+use env_logger::Env;
 
 // #[actix_web::main] // or #[tokio::main]
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    // `init` does call `set_logger`, so this is all we need to do.
+    // We are falling back to printing all logs at info-level or above
+    // if the RUST_LOG environment variable has not been set.
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     // Panic if we can't read configuration
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
--- src/startup.rs
+++ src/startup.rs
@@ -1,7 +1,9 @@
 use crate::routes::{health_check, subscribe};
 
-use actix_web::{web, App, HttpResponse, HttpServer};
 use actix_web::dev::Server;
+use actix_web::web::Data;
+use actix_web::{web, App, HttpServer};
+use actix_web::middleware::Logger;
 use sqlx::PgPool;
 use std::net::TcpListener;
 
@@ -18,6 +20,8 @@ pub fn run(
     // Capture `connection` from the surrounding environment
     let server = HttpServer::new(move || {
         App::new()
+            // Middlewares are added using the `wrap` method on `App`
+            .wrap(Logger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             // Get a pointer copy and attach it to the application state
