e95246997d54f834602017111229a0d742722168
Michael Lagomarsino <mike@radom.network>
2022-06-18 23:00:36 +0100

    Logging messages

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -14,6 +14,16 @@ pub async fn subscribe(
     form: web::Form<FormData>,
     pool: web::Data<PgPool>,
 ) -> HttpResponse {
+    let request_id = Uuid::new_v4();
+    log::info!(
+        "request_id {} - Adding '{}' '{}' as a new subscriber.",
+        request_id,
+        form.email,
+        form.name
+    );
+    log::info!("request_id {} - Saving new subscriber details in the database",
+        request_id
+    );
     // `Result` has two variants: `Ok` and `Err`.
 	// The first for successes, the second for failures.
 	// We use a `match` statement to choose what to do based
@@ -34,9 +44,19 @@ pub async fn subscribe(
     .execute(pool.get_ref())
     .await
     {
-        Ok(_) => HttpResponse::Ok().finish(),
+        Ok(_) => {
+            log::info!(
+                "request_id {} - New subscriber details have been saved",
+                request_id
+            );
+            HttpResponse::Ok().finish()
+        },
         Err(e) => {
-            println!("Failed to execute query: {}", e);
+            log::error!(
+                "request_id {} - Failed to execute query: {:?}",
+                request_id,
+                e
+            );
             HttpResponse::InternalServerError().finish()
         }
     }
