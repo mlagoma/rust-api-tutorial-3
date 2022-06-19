fcfc8048e79350135160d196f820bcd6fd450e03
Michael Lagomarsino <mike@radom.network>
2022-06-19 23:42:39 +0100

    Use tracing instrument macro

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -11,28 +11,24 @@ pub struct FormData {
     name: String
 }
 
+#[tracing::instrument(
+    name = "Adding a new subscriber",
+    skip(form, pool),
+    fields(
+        request_id = %Uuid::new_v4(),
+        subscriber_email = %form.email,
+        subscriber_name= %form.name
+    )
+)]
 pub async fn subscribe(
     form: web::Form<FormData>,
     pool: web::Data<PgPool>,
 ) -> HttpResponse {
-    let request_id = Uuid::new_v4();
-    // Spans, like logs, have an associated level
-    // `info_span` creates a span at the info-level
-    let request_span = tracing::info_span!(
-        "Adding a new subscriber.",
-        %request_id,
-        subscriber_email = %form.email,
-        subscriber_name= %form.name
-    );
-    // Using `enter` in an async function is a recipe for disaster!
-    // Bear with me for now, but don't do this at home.
-    // See the following section on `tracing-futures`
-    let _request_span_guard = request_span.enter(); 
     // We do not call `.enter` on query_span!
     // `.instrument` takes care of it at the right moments
     // in the query future lifetime
     let query_span = tracing::info_span!(
-    "Saving new subscriber details in the database"
+        "Saving new subscriber details in the database"
     );
     // `Result` has two variants: `Ok` and `Err`.
 	// The first for successes, the second for failures.
@@ -53,24 +49,14 @@ pub async fn subscribe(
     // That's when we "exit" the span
 	// We use `get_ref` to get an immutable reference to the `PgConnection`
 	// wrapped by `web::Data`.	// Using the pool as a drop-in replacement	
-    .execute(pool.get_ref())
+    .execute(pool.as_ref())
     // First we attach the instrumentation, then we `.await` it
     .instrument(query_span)
     .await
     {
-        Ok(_) => {
-            tracing::info!(
-                "request_id {} - New subscriber details have been saved",
-                request_id
-            );
-            HttpResponse::Ok().finish()
-        },
+        Ok(_) => HttpResponse::Ok().finish(),
         Err(e) => {
-            tracing::error!(
-                "request_id {} - Failed to execute query: {:?}",
-                request_id,
-                e
-            );
+            tracing::error!("Failed to execute query: {:?}", e);
             HttpResponse::InternalServerError().finish()
         }
     }
