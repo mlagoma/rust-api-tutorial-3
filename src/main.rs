203a0221fef358fc4195417cb54bb7377df637ad
Michael Lagomarsino <mike@radom.network>
2022-06-19 21:46:36 +0100

    Cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,15 @@
 use rust_api_tutorial_3::startup::run;
 use rust_api_tutorial_3::configuration::get_configuration;
+use rust_api_tutorial_3::telemetry::{get_subscriber, init_subscriber};
 
 use sqlx::PgPool;
 use std::net::TcpListener;
-use tracing::subscriber::set_global_default;
-use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
-use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
-use tracing_log::LogTracer;
 
 // #[actix_web::main] // or #[tokio::main]
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    // Redirect all `log`'s events to our subscriber
-    LogTracer::init().expect("Failed to set logger");
-
-    // We removed the `env_logger` line we had before!
-    // We are falling back to printing all spans at info-level or above
-    // if the RUST_LOG environment variable has not been set.
-    let env_filter = EnvFilter::try_from_default_env()
-        .unwrap_or_else(|_| EnvFilter::new("info"));
-    let formatting_layer = BunyanFormattingLayer::new(
-        "rust_api_tutorial_3".into(),
-        // Output the formatted spans to stdout.
-        std::io::stdout
-    );
-    // The `with` method is provided by `SubscriberExt`, an extension
-    // trait for `Subscriber` exposed by `tracing_subscriber`
-    let subscriber = Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer);
-    // `set_global_default` can be used by applications to specify
-    // what subscriber should be used to process spans.
-    set_global_default(subscriber).expect("Failed to set subscriber");
+    let subscriber = get_subscriber("rust_api_tutorial_3".into(), "info".into());
+    init_subscriber(subscriber);
 
     // Panic if we can't read configuration
     let configuration = get_configuration().expect("Failed to read configuration.");
